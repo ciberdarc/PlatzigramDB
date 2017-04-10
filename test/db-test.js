4257932a1ef6ca3fccdb5d3a75ef74e95d7a4ef1
Alexis Araujo <ala1289@gmail.com>
2017-04-10 08:29:46 -0500

    Buenas practicas de creacion de usuarios en Backend

--- test/db-test.js
+++ test/db-test.js
@@ -2,6 +2,7 @@
 
 const test = require('ava')
 const Db = require('../')
+const utils = require('../lib/utils')
 const uuid = require('uuid-base62')
 const r = require('rethinkdb')
 
@@ -78,4 +79,18 @@ test('list all images', async t => {
   let created = await Promise.all(saveImages)
   let result = await db.getImages()
   t.is(created.length, result.length)
+})
+
+test('save user', async t => {
+  let db = t.context.db
+  t.is(typeof db.saveUser, 'function', 'saveUser is function')
+  let user = fixtures.getUser()
+  let plainPassword = user.password
+  let created = await db.saveUser(user)
+
+  t.is(user.username, created.username)
+  t.is(user.name, created.name)
+  t.is(utils.encrypt(plainPassword), created.password)
+  t.is(typeof created.id, 'string')
+  t.truthy(created.createdAt)
 })
